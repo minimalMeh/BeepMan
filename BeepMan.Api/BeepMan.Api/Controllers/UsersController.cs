18c11c6db110f43d0058ee08c97589d062fc2b30
minimalMeh <dasha.osd@gmail.com>
2020-10-06 16:59:10 +0300

    initial commit

---
+++ BeepMan.Api/BeepMan.Api/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace BeepMan.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        public UsersController()
+        {
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return null;
+        }
+    }
+}
