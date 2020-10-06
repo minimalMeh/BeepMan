127147015b5a5f19cfeacc3334d1b326a1c11c90
minimalMeh <dasha.osd@gmail.com>
2020-10-06 19:49:10 +0300

    create customerRepository

--- BeepMan.Api/BeepMan.Api/Servicies/CustomerRepository.cs
+++ BeepMan.Api/BeepMan.Api/Servicies/CustomerRepository.cs
@@ -1,12 +1,52 @@
-﻿using System;
+﻿using BeepMan.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BeepMan.Api.Servicies
 {
-    public class CustomerRepository
+    public class CustomerRepository : IRepository<Customer>
     {
+        private readonly ApplicationDbContext _context;
 
+        public CustomerRepository(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+
+        public void Create(Customer item)
+        {
+            _context.Customers.Add(item);
+        }
+
+        public void Delete(Guid id)
+        {
+            var customer = this.Get(id);
+            if (customer != null)
+            {
+                _context.Customers.Remove(customer);
+            }
+        }
+
+        public Customer Get(Guid id)
+        {
+            return this._context.Customers.FirstOrDefault(i => i.Id == id);
+        }
+
+        public IEnumerable<Customer> GetAll()
+        {
+            return this._context.Customers;
+        }
+
+        public void Update(Customer item)
+        {
+            var exItem = this.Get(item.Id);
+            if(!item.Equals(exItem))
+            {
+                this._context.Customers.Update(exItem);
+            }
+        }
     }
 }
--- BeepMan.Api/BeepMan.Model/Customer.cs
+++ BeepMan.Api/BeepMan.Model/Customer.cs
@@ -3,21 +3,26 @@ using System.ComponentModel.DataAnnotations;
 
 namespace BeepMan.Model
 {
-    public class Customer
+    public class Customer : IEquatable<Customer>
     {
         [Required]
         public Guid Id { get; set; }
 
         [Required]
-
         public string Name { get; set; }
 
         [Required]
-
         public string Phone { get; set; }
 
         [Required]
-
         public virtual Product SelectedProduct { get; set; }
+
+        public bool Equals(Customer other)
+        {
+            return other.Id == this.Id &&
+                other.Name == this.Name &&
+                other.Phone == this.Phone &&
+                other.SelectedProduct == this.SelectedProduct;
+        }
     }
 }
