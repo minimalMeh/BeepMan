bd3d9e12c9d6f775e9530b99461e28656873d0bb
minimalMeh <dasha.osd@gmail.com>
2020-10-07 19:03:32 +0300

    Create unit of work

---
+++ BeepMan.Api/BeepMan.Api/Interfaces/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using BeepMan.Api.ViewModels;
+using BeepMan.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeepMan.Api.Interfaces
+{
+    public interface IUserService
+    {
+        Product CreateProduct(ProductViewModel product);
+
+        void RemoveProduct(ProductViewModel product);
+
+        IList<Customer> GetProductCustomers(Guid productId);
+    }
+}
---
+++ BeepMan.Api/BeepMan.Api/Servicies/UnitOfWork.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using BeepMan.Model;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace BeepMan.Api.Servicies
+{
+    public class UnitOfWork : IDisposable
+    {
+        private bool disposed = false;
+
+        private readonly ApplicationDbContext _context;
+
+        protected UnitOfWork(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+
+        public async Task<bool> ExecuteTransactionAsync(Action action, CancellationToken cancellationToken)
+        {
+            try
+            {
+                using (var transactionScope = new TransactionScope(TransactionScopeOption.Required,
+                    new TransactionOptions() { IsolationLevel = IsolationLevel.Serializable }))
+                {
+                    action();
+                    await this.CommitAsync(cancellationToken);
+                    transactionScope.Complete();
+                    return true;
+                }
+            }
+            catch (TransactionAbortedException e)
+            {
+                Console.WriteLine("Transaction rollback.");
+                throw;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public Task CommitAsync(CancellationToken cancellationToken)
+        {
+            return _context.SaveChangesAsync(cancellationToken);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+                this.disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
