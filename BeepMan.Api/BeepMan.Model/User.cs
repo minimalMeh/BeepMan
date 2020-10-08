206fe7bd3f2fbac398d86df0ca586d893d073875
minimalMeh <dasha.osd@gmail.com>
2020-10-08 19:26:49 +0300

    used Guid as Id

--- BeepMan.Api/BeepMan.Model/User.cs
+++ BeepMan.Api/BeepMan.Model/User.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace BeepMan.Model
 {
-    public class User : IdentityUser
+    public class User : IdentityUser<Guid>
     {
         public virtual ICollection<Product> Products { get; set; }
     }
