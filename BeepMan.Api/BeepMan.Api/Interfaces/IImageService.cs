82719fd6da70608c9b93e026cd2732dee474775d
minimalMeh <dasha.osd@gmail.com>
2020-10-11 20:09:00 +0300

    add IImageService and viewmodel

---
+++ BeepMan.Api/BeepMan.Api/Interfaces/IImageService.cs
@@ -0,0 +1,17 @@
+﻿using BeepMan.Api.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeepMan.Api.Interfaces
+{
+    public interface IImageService
+    {
+        Task<bool> AddImage(Guid productId, ImageViewModel image);
+
+        Task<bool> RemoveImage(Guid imageId);
+
+        IList<ImageViewModel> GetProductImages(Guid productId);
+    }
+}
---
+++ BeepMan.Api/BeepMan.Api/ViewModels/ImageViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeepMan.Api.ViewModels
+{
+    public class ImageViewModel
+    {
+
+        public byte[] ImageBytes { get; set; }
+
+        public string FullName { get; set; }
+    }
+}
